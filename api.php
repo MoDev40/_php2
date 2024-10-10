8dc541761e8ef100df6b3646834b34bca3b657cd
M40 <modev.404@gmail.com>
2024-10-10 15:17:54 +0300

    read all data

---
+++ api.php
@@ -0,0 +1,27 @@
+<?php
+
+include 'connection.php';
+
+header("Content-type:application/json");
+
+function readAllData($conn) {
+    $data = array();
+    $response = array();
+
+    $query = "SELECT * FROM student";
+
+    $result = $conn -> query($query);
+
+    if($result){
+        while($row = $result -> fetch_assoc()){
+            $data [] = $row; 
+        }
+        $response = array("status" => true,"data" => $data);
+    }else {
+        $response = array("status" => false,"data" => $conn ->error);
+    }
+
+    echo json_encode($response);
+}
+
+?>
