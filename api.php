556c4498ece31c53907c6559d86a9bdedcd889fe
M40 <modev.404@gmail.com>
2024-10-12 09:46:32 +0300

    register student

--- api.php
+++ api.php
@@ -26,8 +26,29 @@ function readAllData($conn) {
     echo json_encode($response);
 }
 
+function addStudent ($con) {
+    $response = array();
+    $data = "Successfully added";
+
+    $id= $_POST['student_id'];
+    $name= $_POST['student_name'];
+    $class= $_POST['student_class'];
+    
+    $query = "INSERT INTO student(id,name,class) VALUES('$id','$name','$class')";
+
+    $result = $con -> query($query);
+
+    if($result){
+        $response = array("status"=>true,"data"=> $data);
+    }else {
+        $response = array("status"=>false,"data"=> $con -> error);
+    }
+
+    echo json_encode($response) ;
+}
 
 if(isset($action)){
+    $action($connection);
     $action($connection);
 }
 
