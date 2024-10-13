9c59508aaba14af77552233658b87e4ceeb9ab26
M40 <modev.404@gmail.com>
2024-10-13 09:52:21 +0300

    delete functionality & refactor

--- main.js
+++ main.js
@@ -19,14 +19,14 @@ $("#studentForm").submit((e) => {
     url: "api.php",
     processData: false,
     contentType: false,
-    success: function (data) {
-      const { data: message } = data;
+    success: function () {
       e.target.reset();
-      alert(message);
+      getStudents();
     },
     error: function (xhr, status, error) {
-      alert("AJAX Error: ", status, error);
-      alert("Response Text: ", xhr.responseText);
+      alert(
+        `Delete Error:, status:${status}, error:${error},text:${xhr.responseText}`
+      );
     },
   });
 });
@@ -34,6 +34,7 @@ $("#studentForm").submit((e) => {
 function getStudents() {
   let formData = new FormData();
   formData.append("action", "readAllData");
+
   $.ajax({
     method: "POST",
     data: formData,
@@ -41,6 +42,8 @@ function getStudents() {
     contentType: false,
     processData: false,
     success: function ({ data }) {
+      $("#studentTale tbody").empty();
+
       $.each(data, function (index, item) {
         $("#studentTale tbody").append(`
           <tr class='border-b'>
@@ -49,18 +52,48 @@ function getStudents() {
             <td class='py-3 px-4'>${item.name}</td>
             <td class='py-3 px-4'>${item.class}</td>
             <td class='py-3 px-4'>
-              <button  class='bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600'>Update</button>
-              <button  class='bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 ml-2'>Delete</button>
+              <button class='bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600'>Update</button>
+              <button id="btnDelete" delete_id="${
+                item.id
+              }" class='delete_info bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 ml-2'>Delete</button>
             </td>
           </tr>
         `);
       });
     },
     error: function (xhr, status, error) {
-      console.error("AJAX Error:", status, error);
-      console.error("Response Text:", xhr.responseText);
+      alert(
+        `Delete Error:, status:${status}, error:${error},text:${xhr.responseText}`
+      );
     },
   });
 }
 
 getStudents();
+
+$("#studentTale").on("click", "button.delete_info", function () {
+  let formData = new FormData();
+
+  const id = $(this).attr("delete_id");
+
+  formData.append("id", id);
+  formData.append("action", "deleteStudent");
+
+  if (confirm(`Are you sure you want to delete student with ID: ${id}?`)) {
+    $.ajax({
+      method: "POST",
+      url: "api.php",
+      data: formData,
+      contentType: false,
+      processData: false,
+      success: function () {
+        getStudents();
+      },
+      error: function (xhr, status, error) {
+        alert(
+          `Delete Error:, status:${status}, error:${error},text:${xhr.responseText}`
+        );
+      },
+    });
+  }
+});
